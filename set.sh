006167a3f7701077e334e338b4d0b909a57991cd
marcobir <120011330+marcobir@users.noreply.github.com>
2023-12-29 14:31:26 +0100

    Update set.sh

--- set.sh
+++ set.sh
@@ -11,9 +11,9 @@ sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
 sudo apt install --assume-yes xfce4 desktop-base xfce4-terminal 
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+sudo systemctl disable lightdm.service
 sudo apt remove --assume-yes gnome-terminal
 sudo apt install --assume-yes xscreensaver
-sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
@@ -22,3 +22,10 @@ sudo apt -y install firefox
 sudo apt -y install qbittorrent
 sudo apt install nload
 sudo adduser LOKA chrome-remote-desktop
+printf "\nSetup Completed " >&2 ||
+printf "\nError Occured " >&2
+printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+read -p "Paste Here: " CRP
+su - LOKA -c """$CRP"""
+printf 'Check https://remotedesktop.google.com/access/ \n'
+printf 'Your SUDO Pasword is: 1234 \n'
